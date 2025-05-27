fe3ca76711e389b01309392225755946bbb16e50
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-05-27 15:26:42 +0100

    Criando a mecânica de combate

--- jogo.py
+++ jogo.py
@@ -19,6 +19,17 @@ class Personagem:
     
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNivel: {self.get_nivel()} "
+
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida <= 0:
+            self.__vida = 0
+
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
     
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade) -> None:
@@ -58,6 +69,16 @@ class Jogo:
 
             input("Pressione Enter para atacar...")
             escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial):  ")
+
+            if escolha == '1':
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida, escolha novamente.")
+
+        if self.heroi.get_vida() > = 0:
+            print("\nParabens, você venceu a batalha!")
+        else:
+            print("\nVocê foi derrotado!")
 
 #Criar instancia do jogo e iniciar batalha 
 jogo = Jogo()
